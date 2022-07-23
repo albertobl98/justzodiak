139764195efdaa88a1e90fb9b58add0699fb0c61
albertobl98 <104452559+albertobl98@users.noreply.github.com>
2022-07-23 22:33:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="Just Zodiac">
+    <meta name="description" content="Si notas que estás perdido, este es tu sitio, Conoce tu futuro solo colocando tu signo zodiacal">
+    <meta name="keywords" content="futuro, tarot, magia">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="Spanish">
     <title>Just Zodiac</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="shortcut icon" href="favicon.png">

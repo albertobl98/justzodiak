f9063fb9aa1f7c29498bc0d06f06a5fc41538d02
albertobl98 <104452559+albertobl98@users.noreply.github.com>
2022-07-23 22:11:58 +0200

    Update pdoconfig.php

--- DB/pdoconfig.php
+++ DB/pdoconfig.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'nuepp3ddzwtnggom.chr7pe7iynqr.eu-west-1.rds.amazonaws.com	';
+$host = 'nuepp3ddzwtnggom.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
 $dbname = 'keg1lxv6p4wc3gug';
 $username = 'qgmssdczhqfk2ort';
 $password = 'bk8ih8x1k61ba3rz';

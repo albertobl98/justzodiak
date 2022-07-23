86f3e7db5006ff6d87067b6f8a320b258b656f0d
albertobl98 <104452559+albertobl98@users.noreply.github.com>
2022-07-23 22:32:05 +0200

    Update select.php

--- pages/select.php
+++ pages/select.php
@@ -1,6 +1,7 @@
 <?php
 require '../DB/connection.php';
 try{
+//     Esta variable se llama signos porque la tabla se llama de la misma forma, es la variable a utilizar para que me salga el select con options en el idex
     $signos = $connection->query('SELECT * FROM signos')->fetchAll();
 }catch(Exception $error){
     echo '<p>Lo sentimos, algo ha fallado.</p>';
